acdf441c09f12910abc53747b2b1a338a60a087e
loganhenig <Drummerboy14!>
2022-07-12 16:20:12 -0400

    working compleate nav bar `

--- Skillstorm-P2/src/app/app-routing.module.ts
+++ Skillstorm-P2/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DevicePageComponent } from './device-page/device-page.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PlanPageComponent } from './plan-page/plan-page.component';
+import { UserPageComponent } from './user-page/user-page.component';
+
 
 const routes: Routes = [
   {
@@ -13,6 +16,12 @@ const routes: Routes = [
   },
   {
     path: 'plans', component: PlanPageComponent
+  },
+  {
+    path: 'device', component: DevicePageComponent
+  },
+  {
+    path: 'user', component: UserPageComponent
   },
   //This will handle 404 Errors
   { path: '**', pathMatch: 'full', component: PageNotFoundComponent }
