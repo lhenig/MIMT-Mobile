339457c2ea928d04a2757eedb2872548945eac7d
EddyLeonardoSS <eleonardo@skillstorm.com>
2022-07-15 14:59:09 -0400

    fixed syntax errors

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserPageComponent } from '.components/user-page/user-page.component';
+import { UserPageComponent } from './components/user-page/user-page.component';
 import { PlanPageComponent } from './components/plan-page/plan-page.component';
-import { DevicePageComponent } from '.components/device-page/device-page.component';
-import { LandingPageComponent } from '.components/landing-page/landing-page.component';
-import { PageNotFoundComponent } from '.components/page-not-found/page-not-found.component';
-import { UserInfoComponent } from '.components/user-info/user-info.component';
-import { UsersPlansComponent } from '.components/users-plans/users-plans.component';
+import { DevicePageComponent } from './components/device-page/device-page.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { UsersPlansComponent } from './components/users-plans/users-plans.component';
 
 @NgModule({
   declarations: [
