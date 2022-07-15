0c9bf6fa09f003afd398ea41a44ea446aa2b6308
EddyLeonardoSS <eleonardo@skillstorm.com>
2022-07-15 15:18:08 -0400

    new branch

--- server/src/main/java/com/project2/beans/User.java
+++ server/src/main/java/com/project2/beans/User.java
@@ -21,6 +21,15 @@ public class User {
 	
 	@Column(name = "name")
 	private String name;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "plans")
+	private String plans;
+
+	@Column(name = "devices")
+	private String devices;
 	
 	public User() {}
 	
@@ -32,10 +41,36 @@ public class User {
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPlans() {
+		return plans;
+	}
+
+	public void setPlans(String plans) {
+		this.plans = plans;
+	}
+
+	public String getDevices() {
+		return devices;
+	}
+
+	public void setDevices(String devices) {
+		this.devices = devices;
+	}
+
+	
 	
 	public String toString() {
 		return "User [id=" + id + ", name= " + name + "]/n";
