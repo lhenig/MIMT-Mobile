c6675e0398e1021b8bfaa40f530badefe05e8f77
EddyLeonardoSS <eleonardo@skillstorm.com>
2022-07-15 17:45:54 -0400

    completed models for Plan and Device

--- server/src/main/java/com/project2/beans/Device.java
+++ server/src/main/java/com/project2/beans/Device.java
@@ -10,6 +10,50 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+@Entity
+@Table(name = "devices")
 public class Device {
     
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+    @Column(name = "device_name")
+    private String deviceName;
+
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name = "plan_id")
+    private int planId;
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getPlanId() {
+        return planId;
+    }
+
+    public void setPlanId(int planId) {
+        this.planId = planId;
+    }
+
+    @Override
+    public String toString() {
+		return "Device: [id=" + id + ", name= " + deviceName + ", UserId=" + userId + ", planId=" + planId + "]/n";
+	}
+
 }
--- server/src/main/java/com/project2/beans/Plan.java
+++ server/src/main/java/com/project2/beans/Plan.java
@@ -10,7 +10,48 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-
+@Entity
+@Table(name = "plans")
 public class Plan {
+    
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+    @Column(name = "plan_name")
+    private String planName;
+
+    @Column(name = "device_limit")
+    private int deviceLimit;   
+
+    public Plan() {};
+
+    public Plan( String planName, int deviceLimit) {
+        super();
+        this.planName = planName;
+        this.deviceLimit = deviceLimit;
+    }
+
+    public String getPlanName() {
+        return planName;
+    }
+
+    public void setPlanName(String planName) {
+        this.planName = planName;
+    }
+
+    public int getDeviceLimit() {
+        return deviceLimit;
+    }
+
+    public void setDeviceLimit(int deviceLimit) {
+        this.deviceLimit = deviceLimit;
+    }
+
+    @Override
+    public String toString() {
+		return "Plan: [id=" + id + ", name= " + planName + ", Device Limit=" + deviceLimit + "]/n";
+	}
+
     
 }
