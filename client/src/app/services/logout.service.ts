b54b6d9890897e0d9d2e28c9c2e4ad28341396c3
kkelbachss <kkelbach@skillstorm.com>
2022-07-24 16:59:21 -0400

    logout works WITHCREDENTIALS

--- client/src/app/services/logout.service.ts
+++ client/src/app/services/logout.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +8,7 @@ import { Injectable } from '@angular/core';
 export class LogoutService {
   url: string = "http://localhost:8080/logout"; //dunno yet
   constructor(private http: HttpClient) { }
-  logout() {
-    
-    return this.http.get(this.url, {observe: 'response'});
+  logout(): Observable<HttpResponse<any>> {
+    return this.http.get(this.url, {observe: 'response', withCredentials: true});
   }
 }
